03e5368dd327109782dc7a780ed5daf5f8630f6b
DersWasTaken <isthatder@gmail.com>
2023-11-25 22:06:22 -0700

    [=] Version 1.2 [-] GlowLib (Yay!)

--- src/main/java/me/ders/darknessutils/mixin/GlowingMixin.java
+++ src/main/java/me/ders/darknessutils/mixin/GlowingMixin.java
@@ -2,9 +2,6 @@ package me.ders.darknessutils.mixin;
 
 
 import me.ders.darknessutils.DarknessUtils;
-import me.emafire003.dev.coloredglowlib.ColoredGlowLib;
-import me.emafire003.dev.coloredglowlib.ColoredGlowLibMod;
-import me.emafire003.dev.coloredglowlib.util.Color;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.Entity;
@@ -14,6 +11,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 @Environment(EnvType.CLIENT)
 @Mixin(Entity.class)
-public abstract class GlowingMixin {
+public abstract class GlowingMixin  {
 
     @Shadow public abstract World getWorld();
 
@@ -36,24 +36,22 @@ public abstract class GlowingMixin {
     @Shadow public abstract boolean hasCustomName();
 
     //TODO: MOVE TO CONFIGURATION FILE/POLL API FOR NAMES
-    Map<String, Color> mobColors = new HashMap<>() {{
-        put("Skeleton King", new Color(0xCCCCCC));
-        put("Zombie King", new Color(0x38761D));
-        put("Nether King", new Color(0xB45F06));
-        put("Dragon Slayer", new Color(0xE95BFF));
-        put("Panda King", new Color(0x6AA84F));
-        put("Pumpkin King", new Color(0xB44409));
-        put("Ocean Prince", new Color(0x3D85C6));
-        put("Witch Queen", new Color(0x674EA7));
-        put("Dragon King", new Color(0x1B0F29));
-        put("Super Warden", new Color(0x0B5394));
-        put("Cursed AutismFather", new Color(0xFF0014));
-        put("Cursed ZEDDY1977", new Color(0xFF4F0B));
-        put("Cursed Crankles_", new Color(0xFF059F));
-        put("Cursed Solphire", new Color(0xAA26FF));
+    Map<String, Integer> mobColors = new HashMap<>() {{
+        put("Skeleton King", 0xCCCCCC);
+        put("Zombie King", 0x38761D);
+        put("Nether King", 0xB45F06);
+        put("Dragon Slayer", 0xE95BFF);
+        put("Panda King", 0x6AA84F);
+        put("Pumpkin King",0xB44409);
+        put("Ocean Prince", 0x3D85C6);
+        put("Witch Queen", 0x674EA7);
+        put("Dragon King", 0x1B0F29);
+        put("Super Warden", 0x0B5394);
+        put("Cursed AutismFather", 0xFF0014);
+        put("Cursed ZEDDY1977", 0xFF4F0B);
+        put("Cursed Crankles_", 0xFF059F);
+        put("Cursed Solphire", 0xAA26FF);
     }};
-    //TODO: REMOVE GLOW-LIB AND IMPLEMENT CUSTOM LOGIC
-    ColoredGlowLib lib = ColoredGlowLibMod.getLib();
 
     /**
      * @author Der_s
@@ -61,25 +59,31 @@ public abstract class GlowingMixin {
      */
     @Overwrite
     public boolean isGlowing() {
-        if(hasCustomName() && DarknessUtils.GlowEnabled) {
-            //TODO: SUPER SLOW AND UGLY, FIX LATER
-            Optional<Map.Entry<String, Color>> mobColor = mobColors
-                    .entrySet()
-                    .stream()
-                    .filter(it ->
-                            getCustomName()
-                            .toString()
-                            .toLowerCase()
-                            .contains(it.getKey().toLowerCase())
-                    ).findFirst();
-
-            if(mobColor.isPresent()) {
-                lib.setColorToEntity((Entity) (Object) this, mobColor.get().getValue());
-                return true;
+        if(hasCustomName()) {
+            if(getMobColor().isPresent()) {
+                return DarknessUtils.GlowEnabled;
             }
         }
 
         return getWorld().isClient() ? getFlag(GLOWING_FLAG_INDEX) : glowing;
     }
 
+    @Inject(method = "getTeamColorValue", at = @At("HEAD"), cancellable = true)
+    public void injectGetTeamColorValue(CallbackInfoReturnable<Integer> ci) {
+        if(hasCustomName() && DarknessUtils.GlowEnabled) {
+            getMobColor().ifPresent(stringColorEntry -> ci.setReturnValue(stringColorEntry.getValue()));
+        }
+    }
+
+    public Optional<Map.Entry<String, Integer>> getMobColor() {
+        return mobColors
+                .entrySet()
+                .stream()
+                .filter(it ->
+                        getCustomName()
+                                .toString()
+                                .toLowerCase()
+                                .contains(it.getKey().toLowerCase())
+                ).findFirst();
+    }
 }
